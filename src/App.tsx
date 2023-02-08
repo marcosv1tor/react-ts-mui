a6e3a7ad1129c78b1c6ade8b5b6f25e6bb8f27ce
Marcos Vítor <marcosvitorbc@gmail.com>
2023-02-08 15:18:28 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-  
-      <AppRoutes />
-  
+    <BrowserRouter>  
+      <AppRoutes />  
     </BrowserRouter>
-  )
+  );
 }
