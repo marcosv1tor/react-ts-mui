da96ee15a7dc54f4f54411d2c0ae5cd471dbfef5
Marcos Vítor <marcosvitorbc@gmail.com>
2023-02-08 15:19:05 -0300

    adicionando rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,6 @@ export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
-
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     );
